974db78514f271a103c5f6641922fc563225d02d
Gustavo Estevam <gustavo.estevam@outlook.com>
2022-10-12 18:29:20 -0300

    first commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import styles from './styles.module.scss'
+
+type SubscribeButtonProps = {
+    priceId: string
+}
+
+export const SubscribeButton: FC<SubscribeButtonProps> = ({ priceId }) => {
+    return (
+        <button
+            type="button"
+            className={styles.subscribeButton}
+        >
+            Subscribe
+        </button>
+    )
+}
